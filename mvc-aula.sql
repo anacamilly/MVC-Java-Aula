4214b744e18d170178e129cf3ad171470a7c42b9
Ana Camilly <camillyguedes2@gmail.com>
2022-05-20 17:48:49 -0300

    fourth commit

---
+++ mvc-aula.sql
@@ -0,0 +1,12 @@
+-- Database: mvc-aula
+
+-- DROP DATABASE IF EXISTS "mvc-aula";
+
+CREATE DATABASE "mvc-aula"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
