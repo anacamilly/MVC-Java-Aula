a99a723c2a86a33ad0d3d6ca36e331d75c32aba9
Ana Camilly <camillyguedes2@gmail.com>
2022-05-25 20:55:37 -0300

    fifth commit

--- src/main/java/ufrn/br/demo/controller/ProdutoController.java
+++ src/main/java/ufrn/br/demo/controller/ProdutoController.java
@@ -1,8 +1,15 @@
 package ufrn.br.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import ufrn.br.demo.model.Produto;
 import ufrn.br.demo.service.ProdutoService;
 
 /*@Controller é responsável tanto por receber requisições como por enviar a resposta ao usuário,
@@ -15,10 +22,39 @@ public class ProdutoController {
     public ProdutoController(ProdutoService service) {
         this.service = service;
     }
-
     @RequestMapping(value = {"/", "/produtos"})
     public String getHome(Model model){
-        model.addAttribute("nome", "Hello que veio do código");
+
+        List<Produto> produtos = service.findAll();
+        model.addAttribute("produtosList", produtos);
+
         return "index";
+    }
+
+    @RequestMapping("/cadastrar")
+    public String getCadastar(Model model){
+        Produto p = new Produto();
+        model.addAttribute("produto", p);
+        return "cadastrar";
+    }
+
+    @RequestMapping(value = "/salvar", method = RequestMethod.POST)
+    public String doSalvar(@ModelAttribute Produto p){
+        service.create(p);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/deletar/{id}")
+    public String doDeletar(@PathVariable (name = "id") Long id){
+        service.delete(id);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/editar/{id}")
+    public String doEditar(@PathVariable (name = "id") Long id, Model model){
+
+        Produto p = service.findById(id);
+        model.addAttribute("produto", p);
+        return "cadastrar";
     }
 }
